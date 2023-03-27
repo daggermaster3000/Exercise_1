3ea593667314a08f9834d85c7853e8809645eb67
apasini91 <pasini.alessandro97@gmail.com>
2023-03-27 17:25:13 +0200

    g

--- ex_1.py
+++ ex_1.py
@@ -12,43 +12,32 @@ import GammaTones as gt
 import PySimpleGUI as sg
 
 
+#get parameters for the simulation
+n_electrodes = 20
+freq_lower, freq_upper = (200, 500) #Hz
+win_size = 6e-3 #s
+win_step = 5e-4 #s
+
 #get input filename using GUI
-filename = sg.popup_get_file('', no_window=True)
+def get_input_file():
 
-audioDir = 'Sounds'
-soundFile = os.path.join(audioDir, filename)
-rate, data = read(soundFile)
-print(data)
+    absolute_path = os.path.abspath(__file__)
+    dir_name = os.path.dirname(absolute_path)
+    filename = sg.popup_get_file('', no_window = True, initial_folder = dir_name+"/sounds")
+    
+    rate, data = read(filename)
+    return data, rate
 
-#get settings for the simulation
-n_electrodes = ...
-freq_range = ...
-win_size = ...
-win_type = ...
-sample_rate = ...
+data, rate = get_input_file()
 
-
-
-#gamma tone process input file
-(forward, feedback, fcs, ERB, B) = gt.GT_coefficients(rate, 50, 200, 3000, "moore")
-y = gt.GT_apply(data, forward, feedback)
-
-# Show the plots
-fig, axs = plt.subplots(1, 2)
-
-# Show all frequencies, and label a selection of centre frequencies
-gt.show_basilarmembrane_movement(y, rate, fcs, [0, 9, 19, 29, 39, 49], axs[0])
-
-# For better visibility, plot selected center-frequencies in a second plot.
-# Dont plot the centre frequencies on the ordinate.
-gt.show_basilarmembrane_movement(y[[0, 9, 19, 29, 39, 49], :], rate, [], [], axs[1])
-plt.show()
-plt.close()
+#simulate
+def simulate(data, rate, n_electrodes, freq_lower, freq_upper, win_size, win_step):
+    #gamma tone process input file
+    (forward, feedback, fcs, ERB, B) = gt.GT_coefficients(rate, n_electrodes, freq_lower, freq_upper, "moore")
+    processed_data = gt.GT_apply(data, forward, feedback)
 
 
+    #window input file
+        #map the max energy to the closest electrode
 
-
-#window input file
-    #map the max energy to the closest electrode
-
-#output file and playback
+    #output file and playback
