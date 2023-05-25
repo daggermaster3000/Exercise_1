5ff608583f8e50623797ffe1a04995b7524e55b9
daggermaster3000 <quillan.favey@unil.ch>
2023-05-25 14:11:59 +0200

    hshshs

---
+++ Exercise_3/ex3_v1.py
@@ -0,0 +1,25 @@
+"""
+Exercise 3: Simulation of a "visual prosthesis"
+Authors: Quillan Favey, Alessandro Pasini
+Version: 1.0
+Date: 24.05.2023
+
+This file contains code for simulating a visual prosthesis
+
+OUTPUTS: 
+      FILES                     DESCRIPTION                                         VALUES
+
+
+NAMING CONVENTIONS: 
+
+
+NOTES/USAGE:
+    - Run it from your IDE or alternatively from a command prompt by activating 
+      your environment and running: python Ex2_Vestibular.py
+
+"""
+
+# import libraries
+
+
+
